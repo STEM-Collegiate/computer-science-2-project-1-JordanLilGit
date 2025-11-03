0824e5c2300d491acf7f5aff07f367fe9f0689cb
JordanLilGit <jordanb755@stemcollegiate.ca>
2025-11-03 10:16:57 -0700

    Update pipes.gd

--- pipes.gd
+++ pipes.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 #Set values
-var speed: float = 350
+var speed: float = 400
+@onready var timer: Timer = $Timer
 
 #Function to move forward
 func _physics_process(delta: float) -> void:
 	position.x -= speed * delta
 	
-#Signal to restart game
+#Function to slow down game on death
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.25
+	timer.start()
+	
+#Function to restart game
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
 	
 #Function to increase score
